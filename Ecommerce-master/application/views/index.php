e24952899e048638fa1e6b83d2ac5dc34685f172
Nikki <cndnikki@Chelseas-MBP-2.local.tld>
2015-05-04 14:08:51 -0700

    I have edited the admin controller and admin model

---
+++ Ecommerce-master/application/views/index.php
@@ -0,0 +1,51 @@
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<form class="col-md-6">
+				<h2>Register</h2>
+				<div class="form-group">
+				    <label for="first_name">First Name</label>
+				    <input type="text" class="form-control" id="first_name">
+				</div>
+				<div class="form-group">
+				    <label for="last_name">Last Name</label>
+				    <input type="text" class="form-control" id="last_name">
+				</div>
+				<div class="form-group">
+				    <label for="email">Email address</label>
+				    <input type="email" class="form-control" id="email">
+				</div>
+				<div class="form-group">
+				    <label for="password">Password</label>
+				    <input type="password" class="form-control" id="password">
+				</div>
+				<div class="form-group">
+				    <label for="con_password">Confirm Password</label>
+				    <input type="password" class="form-control" id="con_password">
+				</div>
+				<button type="submit" class="btn btn-default">Submit</button>
+			</form>	
+			<form class="col-md-6">
+				<h2>Login</h2>
+				<div class="form-group">
+				    <label for="email">Email address</label>
+				    <input type="email" class="form-control" id="email">
+				</div>
+				<div class="form-group">
+				    <label for="password">Password</label>
+				    <input type="password" class="form-control" id="password">
+				</div>
+				<button type="submit" class="btn btn-default">Submit</button>
+			</form>	
+		</div>
+	</div>
+</body>
+</html>
